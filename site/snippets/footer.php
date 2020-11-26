b12baa2a071a57d80f9d7a5fb5371c59fdde7b77
Johann Schopplich <mail@johannschopplich.com>
2020-11-26 11:27:34 +0100

    Add link to imprint in footer

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,9 +2,10 @@
 
   <footer class="footer section text-center">
     <img class="pixelated mb-m" src="<?= asset('assets/img/real-troll-avatar.gif')->url() ?>" alt="Avatar von real Troll">
-    <div>
-      <a href="https://realtroll.hpage.com/">Blog von real Troll</a>
-    </div>
+    <nav class="columns is-centered has-gap-m">
+      <a class="column is-narrow" href="https://realtroll.hpage.com/">Blog von real Troll</a>
+      <a class="column is-narrow" href="<?= url('impressum') ?>">Impressum</a>
+    </nav>
   </footer>
 
 <?= js(['@auto'], ['type' => 'module']) ?>
