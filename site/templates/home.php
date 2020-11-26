7ab88caf7ddfe7394b8e1216c9445fa5d738526d
Johann Schopplich <mail@johannschopplich.com>
2020-11-26 10:36:00 +0100

    Add blueprint for homepage

--- site/templates/home.php
+++ site/templates/home.php
@@ -1,7 +1,7 @@
 <?php snippet('header') ?>
 
 <div class="container is-lg mb-xxl">
-  <h1 class="editorial-title"><?= $site->description() ?></h1>
+  <h1 class="editorial-title"><?= $page->text()->kti() ?></h1>
 </div>
 
 <div class="container is-xl is-fullwidth">
