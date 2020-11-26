91685347d8af35185f224b24a3d6cd070942ef50
Johann Schopplich <mail@johannschopplich.com>
2020-11-26 21:04:35 +0100

    Simplify footer nav & add privacy policy

--- site/snippets/footer.php
+++ site/snippets/footer.php
@@ -2,9 +2,10 @@
 
   <footer class="footer section text-center">
     <img class="pixelated mb-m" src="<?= asset('assets/img/real-troll-avatar.gif')->url() ?>" alt="Avatar von real Troll">
-    <nav class="columns is-centered has-gap-m">
-      <a class="column is-narrow" href="https://realtroll.hpage.com/">Blog von real Troll</a>
-      <a class="column is-narrow" href="<?= url('impressum') ?>">Impressum</a>
+    <nav class="h-stacked d-flex justify-content-center">
+      <a href="https://realtroll.hpage.com/">Blog</a>
+      <a href="<?= url('impressum') ?>">Impressum</a>
+      <a href="<?= url('datenschutzerklaerung') ?>">Datenschutzerklärung</a>
     </nav>
   </footer>
 
