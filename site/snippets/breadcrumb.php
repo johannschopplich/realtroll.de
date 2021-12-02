063f14cf8a3fb7781b210e4e62fc5b18952c2256
Johann Schopplich <mail@johannschopplich.com>
2021-12-02 08:59:56 +0100

    refactor: more unocss classes

--- site/snippets/breadcrumb.php
+++ site/snippets/breadcrumb.php
@@ -1,6 +1,6 @@
-<nav class="breadcrumb due-mb-xl">
+<nav class="due-mb-xl">
   <ul class="list-none flex flex-wrap justify-center text-center">
-    <li><a href="<?= $site->homePage()->url() ?>" class="text-primary-400">Startseite</a></li>
+    <li class="after:content-[/] after:text-theme-text after:due-px-s"><a href="<?= $site->homePage()->url() ?>" class="text-primary-400">Startseite</a></li>
     <li><a href="<?= $page->url() ?>" class="text-current"><?= $page->title()->escape() ?></a></li>
   </ul>
 </nav>
