c87a0f181de4b1bfcedc68b0d895e12f68a3d183
Johann Schopplich <mail@johannschopplich.com>
2022-12-17 17:12:13 +0100

    chore: upgrade dependencies

--- site/snippets/breadcrumb.php
+++ site/snippets/breadcrumb.php
@@ -1,6 +1,6 @@
 <nav class="mb-5xl">
   <ul class="list-none flex flex-wrap justify-center text-center">
-    <li class="after:content-['/'] after:text-theme-text after:px-sm"><a href="<?= $site->homePage()->url() ?>" class="text-primary-400">Startseite</a></li>
+    <li class="after:content-[quoted:/] after:text-theme-text after:px-sm"><a href="<?= $site->homePage()->url() ?>" class="text-primary-400">Startseite</a></li>
     <li><a href="<?= $page->url() ?>" class="text-current"><?= $page->title()->escape() ?></a></li>
   </ul>
 </nav>
