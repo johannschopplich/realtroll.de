4ac4659ad6ba70a6b560d797743ae7214f92b578
Johann Schopplich <mail@johannschopplich.com>
2023-05-10 09:32:27 +0200

    refactor: use more Tailwind CSS utils

--- site/snippets/games.php
+++ site/snippets/games.php
@@ -1,12 +1,12 @@
 <?php if ($games = page('spiele')): ?>
-  <ul class="grid sm:grid-cols-2">
+  <ul class="grid md:grid-cols-2">
     <?php foreach ($games->children()->listed() as $game): ?>
       <?php $screenshots = $game->screenshots()->toFiles()->map(fn ($i) => $i->url())->values() ?>
 
-      <li class="game-card"<?= attr(['data-screenshots' => implode('|', $screenshots)], ' ') ?>>
-        <div class="relative game-card-inner">
+      <li class="bg-primary-700 mt-[-2px] md:[&:nth-child(2n+1)>*]:mr-[-2px]"<?= attr(['data-screenshots' => implode('|', $screenshots)], ' ') ?>>
+        <div class="relative grid grid-rows-[1fr_auto] h-full bg-white border-2 border-primary-700 p-3xl transition-transform duration-200 md:p-5xl md:hover:translate-[-4px] md:hover:active:translate-0">
           <?php if ($logo = $game->logo()->toFile()): ?>
-            <figure class="game-logo mb-4xl">
+            <figure class="mb-4xl">
               <img
                 class="pixelated mx-auto"
                 src="<?= $logo->url() ?>"
