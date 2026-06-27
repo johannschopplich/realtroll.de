8e516b7451ab5393d88d3098c100482cf985e386
Johann Schopplich <johann@schopplich.com>
2026-06-27 08:30:14 +0200

    fix: update post link to use blog URL directly

--- site/snippets/components/devlog.php
+++ site/snippets/components/devlog.php
@@ -4,8 +4,6 @@ $blog = page('blog');
 $post = $blog?->children()->listed()->sortBy('date', 'desc')->first();
 if (!$post) return;
 
-$href = $blog->url() . '#' . $post->slug();
-
 ?>
 <p class="group relative flex flex-wrap items-center justify-center mt-2xl gap-x-3 gap-y-2 max-w-full text-sm">
   <span class="label-caps inline-flex shrink-0 items-center gap-2 px-2 py-1 text-xs leading-none text-primary-800 border-2 border-primary-700">
@@ -13,7 +11,7 @@ $href = $blog->url() . '#' . $post->slug();
     Neues
   </span>
   <time class="hidden shrink-0 text-contrast-medium sm:inline" datetime="<?= $post->date()->toDate('c') ?>"><?= $post->date()->toDate('d.m.Y') ?></time>
-  <a href="<?= $href ?>" class="link-primary static min-w-0 text-sm after:absolute after:inset-0 after:content-['']">
+  <a href="<?= $blog->url() ?>" class="link-primary static min-w-0 text-sm after:absolute after:inset-0 after:content-['']">
     <span class="link-default [--un-decoration-color:transparent] min-w-0 max-w-[20rem] truncate group-hover:decoration-current"><?= $post->title()->escape() ?></span>
   </a>
 </p>
